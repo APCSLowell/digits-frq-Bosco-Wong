f39bf156012159b695b4601e11db527f39a0e378
Bosco-Wong <142552491+Bosco-Wong@users.noreply.github.com>
2024-04-10 04:40:07 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -5,15 +5,21 @@ public class Digits
 
 	private ArrayList<Integer> digitList;
 
-	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
-	}
+	public Digits(int num){
+	int temp = num;
+		for(int x = digitList.size()-1; x >= 0; x++){
+	if(temp > 0){
+digitList.add(x, temp%10);
+temp = temp/10;
+}
+}
+}
 
-	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
-
+	pubic boolean isStrictlyIncreasing(){
+for(int x = 1; x < digitList.size()-1; x++)
+if(digitList.get(x) < digitList.get(x-1))
+return false;
+return true;
 	}
 	
 	public String toString()
